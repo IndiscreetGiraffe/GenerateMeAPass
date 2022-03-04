1d92ffb4c9d12321829b9cfa505cf15509a32941
Kyleah Justice <kikij432@gmail.com>
2022-03-04 02:00:09 -0700

    added the chracters for the password

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,11 @@
 // Assignment code here
 
+//special characters and other input for password
+lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'];
+upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z'];
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+special = ["!","@","#","$","%","^","&","*","(",")","_","+","=",";",":"];
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
