41891bf23a70b0c88d6917b0077fbb36a08c1afc
Kyleah Justice <kikij432@gmail.com>
2022-03-04 02:01:38 -0700

    adding the var for the characters when confirming in the promts

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,8 @@
 // Assignment code here
-
+var confirmLower;
+var confirmUpper;
+var confirmNumbers;
+var confirmSpecial;
 //special characters and other input for password
 lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'];
 upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z'];
