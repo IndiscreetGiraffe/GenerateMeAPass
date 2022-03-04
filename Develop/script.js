1bca4023d02e8b7726bc1b9c0ea0cc16c3d18e37
Kyleah Justice <kikij432@gmail.com>
2022-03-04 02:13:03 -0700

    added the validation for how many characters, and it works in the html. conintuing onto the rest

--- Develop/script.js
+++ Develop/script.js
@@ -3,11 +3,23 @@ var confirmLower;
 var confirmUpper;
 var confirmNumbers;
 var confirmSpecial;
+var amount;
 //special characters and other input for password
 lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'];
 upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z'];
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 special = ["!","@","#","$","%","^","&","*","(",")","_","+","=",";",":"];
+
+//Generate Password code
+function generatePassword() {
+  amount = prompt("How many characters are going to be in your password? Choose between 8 and 128 characters.");
+  //first if to start user validation
+  if (!amount) {
+    alert("Please enter a number to continue");
+  } else if (amount <8 || amount > 128) {
+    alert("Choose a number between 8 and 128 for your password.");
+  }
+}
 
 
 // Get references to the #generate element
